d5ae3e179874d0b5b884bef19a15a2813a40e83b
LaurieAnne <42545271+LaurieAnne@users.noreply.github.com>
2018-11-15 21:46:32 -0500

    premier push

---
+++ Generateur/Generateur.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generateur
+{
+    public class Generateur
+    {
+        FormGenerateur m_interface; //Interface du générateur
+        Scenario m_scenario; //Façade/Médiateur
+
+        [STAThread]
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Generateur m_generateur = new Generateur();
+        }
+
+        public Generateur() //Constructeur
+        {
+            m_scenario = new Scenario();
+            m_interface = new FormGenerateur(this, m_scenario);
+            Application.Run(m_interface);
+        }
+    }
+}
